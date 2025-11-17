2093a83ca4a7eee7e0e80dd6d281af7873de558a
dfn96304 <s30496@pjwstk.edu.pl>
2025-11-17 15:57:31 +0100

    Improve tests

--- src/BYT/Tests/VeganTest.java
+++ src/BYT/Tests/VeganTest.java
@@ -1,5 +1,6 @@
 package BYT.Tests;
 
+import BYT.Classes.MenuItem.MenuItem;
 import BYT.Classes.MenuItem.Normal;
 import BYT.Classes.MenuItem.Vegan;
 import org.junit.jupiter.api.Assertions;
@@ -29,5 +30,40 @@ public class VeganTest extends TestBase<Vegan> {
         List<Vegan> vegan = new ArrayList<>();
         vegan.add(new Vegan("Citrus-Brined Olives", "Marinated mixed olives with orange zest and herbs", 7, "PLABC123"));
         testPersistence(vegan);
+    }
+
+    @Test
+    void testVeganCertificationID(){
+        Vegan vegan = new Vegan("Citrus-Brined Olives", "Marinated mixed olives with orange zest and herbs", 1, "ABC123");
+        assertEquals(vegan.getCertificationID(), "ABC123");
+    }
+
+    @Test
+    void nullCertificationIDThrows() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            Vegan vegan = new Vegan("Citrus-Brined Olives", "Marinated mixed olives with orange zest and herbs", 1, null);
+        });
+    }
+
+    @Test
+    void setNullCertificationIDThrows() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            Vegan vegan = new Vegan("Citrus-Brined Olives", "Marinated mixed olives with orange zest and herbs", 1, "ABC");
+            vegan.setCertificationID(null);
+        });
+    }
+
+    @Test
+    void emptyCertificationIDThrows() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            Vegan vegan = new Vegan("Citrus-Brined Olives", "Marinated mixed olives with orange zest and herbs", 1, "");
+        });
+    }
+    @Test
+    void setEmptyCertificationIDThrows() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            Vegan vegan = new Vegan("Citrus-Brined Olives", "Marinated mixed olives with orange zest and herbs", 1, "1643A");
+            vegan.setCertificationID("");
+        });
     }
 }

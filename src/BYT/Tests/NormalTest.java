322d06823ab015a904d0578333c4df0ba325dbe2
sher304 <esherovshermat@gmail.com>
2025-11-29 20:17:59 +0100

    Normal food test updated

--- src/BYT/Tests/NormalTest.java
+++ src/BYT/Tests/NormalTest.java
@@ -1,16 +1,20 @@
 package BYT.Tests;
 
+import BYT.Classes.Menu.Menu;
+import BYT.Classes.MenuItem.MenuItem;
 import BYT.Classes.MenuItem.Normal;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
 public class NormalTest extends TestBase<Normal> {
+    private Menu testMenu;
 
     protected NormalTest() {
         super(Normal.class);
@@ -19,18 +23,19 @@ public class NormalTest extends TestBase<Normal> {
     @BeforeEach
     void setUp() {
         clearExtentInMemoryList();
+        testMenu = new Menu(LocalDate.now(), LocalDate.now().plusDays(5));
     }
 
     @Test
     void testNormalPersistence_SavingAndLoading() throws IOException, ClassNotFoundException {
         List<Normal> normal = new ArrayList<>();
-        normal.add(new Normal("Citrus-Brined Olives", "Marinated mixed olives with orange zest and herbs", 7));
+        normal.add(new Normal("Citrus-Brined Olives", "Marinated mixed olives with orange zest and herbs", 7, testMenu));
         testPersistence(normal);
     }
 
     @Test
     void testMeatTypeList() {
-        Normal normal = new Normal("Citrus-Brined Olives", "Marinated mixed olives with orange zest and herbs", 7);
+        Normal normal = new Normal("Citrus-Brined Olives", "Marinated mixed olives with orange zest and herbs", 7, testMenu);
         normal.addMeatType("Beef");
         normal.addMeatType("Chicken");
 
